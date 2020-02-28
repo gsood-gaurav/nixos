b6da7ca8389a44de5f3042fd113331c0aa1885d0
Gaurav Sood <gsood.gaurav@gmail.com>
2020-02-28 09:01:57 +0530

    Modified config.nix

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,6 +1,15 @@
 {
-  packageOverrides = super: let self = super.pkgs; in {
-    myemacs = import ./emacsconfig/emacs.nix { pkgs = self; };
+  packageOverrides = pkgs: with pkgs; rec {
+    myemacs = import ./emacs.nix { inherit pkgs; };
+    myPackages = pkgs.buildEnv {
+      name = "my-packages";
+      paths = [
+      direnv
+      chromium
+      myemacs
+      gnomeExtensions.dash-to-dock
+      ];
+    };
   };
 
   allowUnfree = true;
